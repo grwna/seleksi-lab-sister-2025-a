eaf96057b71bee1a7dea111173947903adbccb47
grwna <13523035@std.stei.itb.ac.id>
2025-07-24 19:21:33 +0700

    add file signing and submission

--- submission.py
+++ submission.py
@@ -6,19 +6,23 @@ import json
 import sys
 import pyotp
 import random
+import dotenv
+import os
 
-# THIS IS JUST A TEMPLATE THAT THE SERVER STORES
+dotenv.load_dotenv()
+
 creds = {
-    "username": "13523035",
-    "password": "rayhan",
-    "totp_secret": "aerith",
-    "pow_prefix": "13523035:if:rayhan",
+    "username": os.getenv('USERNAME'),
+    "password": os.getenv('PASSWORD'),
+    "totp_secret": os.getenv('TOTP_SECRET'),
+    "pow_prefix": os.getenv('POW_PREFIX'),
     "status": "pending"
 }
 
 url = "http://104.214.186.131:8000"
 
 sphincs = pyspx.sha2_256f   
+
 
 def find_nonce():
     pow_prefix = creds["pow_prefix"]
@@ -35,6 +39,7 @@ def find_nonce():
             return nonce
         nonce += 1
 
+
 def generate_keys():
         public_key, private_key = sphincs.generate_keypair(random.randbytes(96))
         
@@ -44,6 +49,7 @@ def generate_keys():
         print(f" Length: {int.from_bytes(private_key).bit_length()}, Hash Length: {len(private_key.hex())}" )
         
         return public_key, private_key
+
 
 def validate_keys(pub, priv):
     try:
@@ -55,10 +61,11 @@ def validate_keys(pub, priv):
         print(f"Key pair is INVALID. Error: {e}")
         exit()
 
+
 def activate_account(nonce, public_key):
     payload = {
-        "username": "13523035",
-        "password": "rayhan",
+        "username": creds["username"],
+        "password": creds["password"],
         "nonce":nonce,
         "public_key": base64.b64encode(public_key).decode('utf-8')
     }
@@ -69,34 +76,6 @@ def activate_account(nonce, public_key):
 
     return response
 
-def read_and_sign_file(tahap : int, privkey):
-    path = f"bagian-a/13523035_A_{tahap}.pdf"
-    pdf = open(path, "rb").read()
-
-    signature = sphincs.sign(pdf, privkey)
-    signature_b64 = base64.b64encode(signature).decode('utf-8')
-    pdf_b64 = base64.b64encode(pdf).decode('utf-8')
-
-    open(f"submissions/bagian-a/tahap{tahap}pdf", "w").write(pdf_b64)
-    open(f"submissions/bagian-a/tahap{tahap}sign", "w").write(signature_b64)
-
-    return pdf_b64, signature_b64
-
-def validate_file(file_b64, signature_b64, public_key):
-    pdf = base64.b64decode(file_b64)
-    signature = base64.b64decode(signature_b64)
-
-    try:
-        sphincs.verify(pdf, signature, public_key)
-        print("File signature is valid!")
-    except Exception as e:
-        print(f"File signature is INVALID. Error: {e}")
-        exit()
-
-def construct_pdf(b64_string):
-    path = "submissions/test/" + random.randbytes(6).hex() + ".pdf"
-    pdf = base64.b64decode(b64_string)
-    open(path, "wb").write(pdf)
 
 def update_pubkey(newpubkey):
 
@@ -106,8 +85,8 @@ def update_pubkey(newpubkey):
 
     _, question, answer = get_math()
     payload = {
-        "username": "13523035",
-        "password": "rayhan",
+        "username": creds["username"],
+        "password": creds["password"],
         "totp_code": totp_code,
         "math_question": question,
         "math_answer": answer,
@@ -118,37 +97,82 @@ def update_pubkey(newpubkey):
     print(f"Status: {response.status_code}")
     print(f"Response: {response.json()}")
 
-def submit_a(tahap: int, privkey):
+
+def read_and_sign_file(tahap : int, privkey):
+    path = f"bagian-a/{creds['username']}_A_{tahap}.pdf"
+    pdf = open(path, "rb").read()
+
+    signature = sphincs.sign(pdf, privkey)
+    signature_b64 = base64.b64encode(signature).decode('utf-8')
+
+    open(f"submissions/bagian-a/{creds["username"]}_A_{tahap}.pdf", "wb").write(pdf)
+    open(f"submissions/bagian-a/{creds["username"]}_A_{tahap}.pdf.sign", "w").write(signature_b64)
+
+    return pdf, signature_b64
+
+
+def validate_file(pdf_data, signature_b64, public_key):
+    signature = base64.b64decode(signature_b64)
+
+    try:
+        if not sphincs.verify(pdf_data, signature, public_key):
+            raise Exception("Signature verification failed")
+
+        print("File signature is valid!")
+        return True
+    except Exception as e:
+        print(f"File signature is INVALID. Error: {e}")
+        exit()
+
+
+def submit_a(tahap: int, privkey, pubkey):
     assert tahap == 1 or tahap == 2
 
     pdf, signature = read_and_sign_file(tahap, privkey)
+    validate_file(pdf, signature, pubkey)
     
     secret = base64.b32encode(creds["totp_secret"].encode()).decode()
     totp = pyotp.TOTP(secret)
     totp_code = totp.now()
 
     _, question, answer = get_math()
-    payload = {
-        "username": "13523035",
+    data = {
+        "username": creds["username"],
         "totp_code": totp_code,
         "math_question": question,
         "math_answer": answer,
         "signature": signature,
         "tahap": tahap,
-        "file": base64.b64decode(pdf)
     }
 
-    # Print truncated version for debugging
-    debug_payload = payload.copy()
-    debug_payload["file"] = pdf[:100] + "..." if len(pdf) > 100 else pdf
+    file_data = {
+        'file' : (f'{creds['username']}_A_{tahap}.pdf', pdf, 'application/pdf')
+    }
+
+    debug_payload = data.copy()
     debug_payload["signature"] = signature[:100] + "..." if len(signature) > 100 else signature
     print(json.dumps(debug_payload, indent=2))
 
-    # response = requests.post(url + "/stage-a/submit", json=payload)
-    # print(f"Status: {response.status_code}")
-    # print(f"Response: {response.json()}")
+    response = requests.post(url + "/stage-a/submit", data=data, files=file_data)
+    print(f"Status: {response.status_code}")
+    print(f"Response: {response.json()}")
 
-    # return response
+    return response
+
+
+def get_submissions():
+    secret = base64.b32encode(creds["totp_secret"].encode()).decode()
+    totp = pyotp.TOTP(secret)
+    totp_code = totp.now()
+
+    params = {
+        "username": creds['username'],
+        "totp_code": totp_code
+    }
+
+    response = requests.get(url + f"/user/{creds['username']}/submissions", params=params)
+    print(f"Status: {response.status_code}")
+    print(f"Response: {response.json()}")
 
 
 def get_math():
@@ -156,6 +180,7 @@ def get_math():
     question = response.json()["question"] 
     answer = eval(question.split("||")[0])
     return response, question, answer
+
 
 def get_accounts():
     response = requests.get(url + "/accounts")
@@ -168,13 +193,16 @@ def get_accounts():
     print(f"Total: {len(active_list)}")
     return response
      
+
 def get_stats():
     response = requests.get(url + "/stats")
     return response
 
+
 def get_health():
     response = requests.get(url + "/health")
     return response
+
 
 if __name__ == "__main__":
 
@@ -211,12 +239,8 @@ if __name__ == "__main__":
     if "HEALTH" in sys.argv:
         res = get_health()
     
-    if "PDFTEST" in sys.argv:
-        b64_string = open(sys.argv[2], "r").read()
-        construct_pdf(b64_string)
-    
     if "SIGNTEST" in sys.argv:
-        pdf = open(sys.argv[2], "r").read()
+        pdf = open(sys.argv[2], "rb").read()
         signature = open(sys.argv[3], "r").read()
         validate_file(pdf, signature, public_key)
 
@@ -225,5 +249,8 @@ if __name__ == "__main__":
     
     if "SUBMIT" in sys.argv:
         if sys.argv[2] == "A":
-            res = submit_a(int(sys.argv[3]), private_key)
+            res = submit_a(int(sys.argv[3]), private_key, public_key)
         open("safe/submit_log.txt", "w").write(json.dumps(res.json(), indent=4, sort_keys=True))
+
+    if "CHECKSUB" in sys.argv:
+        get_submissions()
